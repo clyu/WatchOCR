f749fce067a31686927c3175818471607fd03c4c
Leon Yu <5331469+clyu@users.noreply.github.com>
2026-07-29 15:59:22 +0800

    drop the unused compose ui-tooling and ui-tooling-preview dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,10 +105,8 @@ dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2024.09.02"))
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-    debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
